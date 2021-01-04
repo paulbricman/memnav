b88a06bc3d1c075e02cdf9d5572a75625e8d6036
paubric <paubric@gmail.com>
2021-01-04 12:58:38 +0200

    Update

--- memnav.py
+++ memnav.py
@@ -4,9 +4,17 @@ import os
 
 class MemNav:
     def __init__(self, root_dir='.'):
+        self.root_dir = root_dir
         self.qa = pipeline('question-answering')
-        self.context = ' '.join([open(root_dir + '/' + file).read()
+        self.sum = pipeline('summarization')
+        self.context = ' '.join([open(self.root_dir + '/' + file).read()
                                  for file in sorted(os.listdir(root_dir))])
 
     def ask(self, question):
         return self.qa(question, self.context)['answer']
+
+    def sum(self, file):
+        print('Flag')
+        print(self.root_dir + '/' + file)
+        print(open(self.root_dir + '/' + file).read())
+        return self.sum(open(self.root_dir + '/' + file).read(), 130, 30, False)
